442ec8c9621af72fca6556f8fc5480197105e34a
Darya Baranouskaya <darya.baranovskaya01082001@gmail.com>
2020-07-31 12:29:34 +0000

    JSON

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -99,3 +99,25 @@ async function sayHelloUsingAsyncAwait() {
   const quote = await response.text();
   document.getElementById('hello-sell').innerText = quote;
 }
+
+function getServerStats() {
+  fetch('/data').then(response => response.json()).then((jsonStr) => {
+    // stats is an object, not a string, so we have to
+    // reference its fields to create HTML content
+
+    const statsListElement = document.getElementById('hello-sell');
+    statsListElement.innerHTML = '';
+    statsListElement.appendChild(
+        createListElement('First string: ' + jsonStr[0]));
+    statsListElement.appendChild(
+        createListElement('Second string: ' + jsonStr[1]));
+    statsListElement.appendChild(
+        createListElement('Third string: ' + jsonStr[2]));
+  });
+}
+
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
+}
